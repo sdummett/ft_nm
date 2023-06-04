f11c0d4aa94cd1124eed94bb8970a3caf7b6d378
Stone <sdummett.dev@proton.me>
2023-06-04 18:29:46 +0200

    Handle short options, iterate over all the files passed as arguments, if no files are passed, default is a.out

--- srcs/main.c
+++ srcs/main.c
@@ -4,7 +4,8 @@
 #define OPTION_P (1 << 2)
 #define OPTION_R (1 << 3)
 #define OPTION_U (1 << 4)
-#define OPTION_ERROR (1 << 5)
+#define OPTION_H (1 << 5)
+#define OPTION_ERROR (1 << 6)
 
 // DONT FORGET STDERR WHILE PARSING OPTION
 
@@ -29,6 +30,7 @@ Option* get_options(int *count) {
 		{'p', "no-sort"},
 		{'r', "reverse-sort"},
 		{'u', "undefined-only"},
+		{'h', "help"},
     };
 
 	if (count)
@@ -46,7 +48,7 @@ bool is_option_set(char *options, char option_flag) {
 
 void process_long_opt(char *arg, char *opts, char *progname) {
 	int count;
-	Option* options = get_options(&count);
+	Option *options = get_options(&count);
 
     for (long i = 0; i < count; i++) {
 		if (strcmp(arg + 2, options[i].long_opt) == 0) {
@@ -61,9 +63,9 @@ void process_long_opt(char *arg, char *opts, char *progname) {
 				opt_flag = OPTION_R;
 			else if (options[i].short_opt == 'u')
 				opt_flag = OPTION_U;
-			else 
+			else if (options[i].short_opt == 'h')
+				opt_flag = OPTION_H;
 			set_options(opts, opt_flag);
-			printf("%s matched\n", arg);
 			return;
 		}
     }
@@ -75,6 +77,38 @@ void process_long_opt(char *arg, char *opts, char *progname) {
 
 void process_short_opt(char *arg, char *opts, char *progname) {
 	(void)opts; (void)progname;
+	int count;
+	Option *options = get_options(&count);
+
+	int arg_len = strlen(arg); //strlen is forbidden
+	for (int i = 1; i < arg_len; i++) {
+		for (int j = 0; j < count; j++) {
+			if (arg[i] == options[j].short_opt) {
+				char opt_flag = 0;
+				if (options[j].short_opt == 'a')
+					opt_flag = OPTION_A;
+				else if (options[j].short_opt == 'g')
+					opt_flag = OPTION_G;
+				else if (options[j].short_opt == 'p')
+					opt_flag = OPTION_P;
+				else if (options[j].short_opt == 'r')
+					opt_flag = OPTION_R;
+				else if (options[j].short_opt == 'u')
+					opt_flag = OPTION_U;
+				else if (options[j].short_opt == 'h')
+					opt_flag = OPTION_H;
+				set_options(opts, opt_flag);
+				break;
+			}
+			else if (j + 1 == count) {
+				set_options(opts, OPTION_ERROR);
+				// // stderr
+				// printf("%s invalid option -- '%s'\n",progname, arg);
+				printf("%s: unrecognized option '%c'\n", progname, arg[i]);
+				return;
+			}
+		}
+	}
 	printf("short_opt: %s\n", arg);
 }
 
@@ -87,10 +121,14 @@ void get_opt(int ac, char **av, char *opts) {
 	// if one option is unknown, quit and print help()
 	// store the options (or unknown opt) in a char using bitwise operation
 	for (int i = 1; i < ac; i++){
-		if (is_long_opt(*(av + i)))
+		if (is_long_opt(*(av + i))) {
 			process_long_opt(*(av + i), opts, *av);
-		else if (is_short_opt(*(av + i)))
+			av[i][0] = '\0';
+		}
+		else if (is_short_opt(*(av + i))) {
 			process_short_opt(*(av + i), opts, *av);
+			av[i][0] = '\0';
+		}
 		if (is_option_set(opts, OPTION_ERROR))
 			break;
 	}
@@ -106,49 +144,31 @@ void help(char *progname) {
 	printf("-p, --no-sort          Do not sort the symbols\n");
 	printf("-r, --reverse-sort     Reverse the sense of the sort\n");
 	printf("-u, --undefined-only   Display only undefined symbols\n");
+	printf("-h, --help             Display this information\n");
 }
 
-int main(int ac, char **av) {
-	// getopt
-	// loopoverfiles
-	// help(*av);
-	char opts;
-	get_opt(ac, av, &opts);
-
-	if (is_option_set(&opts, OPTION_A))
-		printf("OPTION A SET\n");
-	if (is_option_set(&opts, OPTION_G))
-		printf("OPTION G SET\n");
-	if (is_option_set(&opts, OPTION_P))
-		printf("OPTION P SET\n");
-	if (is_option_set(&opts, OPTION_R))
-		printf("OPTION R SET\n");
-	if (is_option_set(&opts, OPTION_U))
-		printf("OPTION U SET\n");
-	if (is_option_set(&opts, OPTION_ERROR)) {
-		help(*av);
-		return 0;
+int get_nb_files(int ac, char **av) {
+	int count = 0;
+	for (int i = 1; i < ac; i++) {
+		if (av[i][0] != '\0')
+			count++;
 	}
+	return count;
+}
 
-	if (ac != 2)
-	{
-		printf("You must pass an executable, an object file or a library\n");
-		return 0;
-	}
-
-	int fd = open(av[1], O_RDONLY);
-	if (fd == -1)
-	{
-		perror(av[1]);
-		return 0;
+void process_file(char *filename, char *opts) {
+	(void)opts;
+	int fd = open(filename, O_RDONLY);
+	if (fd == -1) {
+		perror(filename); //perror forbidden
+		return;
 	}
 
 	struct stat sb;
 
-	if (fstat(fd, &sb) == -1)
-	{
-		perror("couldnt get file size");
-		return 0;
+	if (fstat(fd, &sb) == -1) {
+		perror("couldnt get file size"); // perror forbidden
+		return;
 	}
 	//	printf("File size of '%s' is %ld bytes\n", av[1], sb.st_size);
 
@@ -172,5 +192,43 @@ int main(int ac, char **av) {
 	}
 	munmap(f, sb.st_size);
 	close(fd);
+}
+
+int main(int ac, char **av) {
+	char opts;
+	get_opt(ac, av, &opts);
+
+	if (is_option_set(&opts, OPTION_A))
+		printf("OPTION A SET\n");
+	if (is_option_set(&opts, OPTION_G))
+		printf("OPTION G SET\n");
+	if (is_option_set(&opts, OPTION_P))
+		printf("OPTION P SET\n");
+	if (is_option_set(&opts, OPTION_R))
+		printf("OPTION R SET\n");
+	if (is_option_set(&opts, OPTION_U))
+		printf("OPTION U SET\n");
+	if (is_option_set(&opts, OPTION_H)) {
+		printf("OPTION_H SET\n");
+		help(*av);
+		return 0;
+	}
+	if (is_option_set(&opts, OPTION_ERROR)) {
+		help(*av);
+		return 0;
+	}
+
+	int nb_files = get_nb_files(ac, av);
+	if (nb_files == 0) {
+		process_file("a.out", &opts);
+		return 0;
+	}
+
+	for (int i = 1; i < ac; i++) {
+		if (av[i][0] == '\0')
+			continue;
+		process_file(av[i], &opts);
+	}
+
 	return 0;
 }
